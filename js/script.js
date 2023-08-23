6c6d67415126344f8dfbe132097b10be59aa3a67
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-08-23 10:40:14 +0000

    Set up structure of game functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("connected");
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
