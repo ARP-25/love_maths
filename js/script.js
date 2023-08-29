62b840f46c1a6171f75bf6956c4a444ed4b03b91
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-08-29 09:52:23 +0000

    Add code for subtraction game

--- js/script.js
+++ js/script.js
@@ -29,7 +29,10 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -65,11 +68,13 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"]
     } else if (operator ==="x") {
         return [operand1 * operand2, "multiply"]
+    } else if (operator ==="-") {
+        return [operand1 - operand2, "subtract"]
     }
-        else {
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
-        }
+    }
 }
 
 
@@ -87,8 +92,15 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
 
+    if (operand1 >= operand2) {
+        document.getElementById("operand1").textContent = operand1;
+        document.getElementById("operand2").textContent = operand2;
+        document.getElementById("operator").textContent = "-";
+    } else {
+        runGame("subtract");
+    }
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
